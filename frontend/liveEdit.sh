4022377ded71edaff731aa004c7e8972c4253afe
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-20 21:03:48 +0200

    Add database forward to liveEdit script
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- frontend/liveEdit.sh
+++ frontend/liveEdit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker rm -f yamba 2>/dev/null;
-sudo docker run -v $(realpath ./public_html):/var/www/html -d --name yamba -p 80:80 yamba:$(sudo docker images | grep "^yamba" | awk ' {print $2}' | head -n 1);
+sudo docker run -v $(realpath ./public_html):/var/www/html -d --net="host" --name yamba -p 80:80 yamba:$(sudo docker images | grep "^yamba" | awk ' {print $2}' | head -n 1);
