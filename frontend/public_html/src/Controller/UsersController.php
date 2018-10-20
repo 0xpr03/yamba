1528e8069a54d7fac7df911a08945194a1d12417
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-20 18:09:40 +0200

    Implement login
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

---
+++ frontend/public_html/src/Controller/UsersController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tony
+ * Date: 20.10.18
+ * Time: 15:21
+ */
+namespace App\Controller;
+use Cake\Event\Event;
+class UsersController extends AppController
+{
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+        $this->Auth->allow(['add', 'logout']);
+    }
+    public function index()
+    {
+        $this->set('users', $this->Users->find('all'));
+    }
+    public function view($id)
+    {
+        $user = $this->Users->get($id);
+        $this->set(compact('user'));
+    }
+    public function add()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            // Prior to 3.4.0 $this->request->data() was used.
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(['action' => 'add']);
+            }
+            $this->Flash->error(__('Unable to add the user.'));
+        }
+        $this->set('user', $user);
+    }
+    public function login()
+    {
+        if ($this->request->is('post')) {
+            $user = $this->Auth->identify();
+            if ($user) {
+                $this->Auth->setUser($user);
+                return $this->redirect($this->Auth->redirectUrl());
+            }
+            $this->Flash->error(__('Invalid username or password, try again'));
+        }
+    }
+    public function logout()
+    {
+        return $this->redirect($this->Auth->logout());
+    }
+}
