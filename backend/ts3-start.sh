7f12462713513ab08e31ac0449d903d2686989f8
Oliver Habersetzer <oliver.habersetzer@gmail.com>
2018-10-20 22:07:33 +0200

    Forward all cmdline args to ts3-runscript

--- backend/ts3-start.sh
+++ backend/ts3-start.sh
@@ -8,4 +8,4 @@ killall -9 Xvfb;
 echo "Starting Xvfb with ts3 on display #$display... ";
 /usr/bin/Xvfb :$display &
 export DISPLAY=:$display.0
-./ts3client/ts3client_runscript.sh
+./ts3client/ts3client_runscript.sh $@
