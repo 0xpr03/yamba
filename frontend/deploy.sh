0b7ba5a81d76c6576efba43bad9cf72f3b17d49d
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-20 15:01:06 +0200

    Add composer install to deploy
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,5 +2,8 @@
 git pull && version=$(git rev-parse HEAD) || version=testing;
 echo "Version=${version}";
 sudo docker rm -f yamba 2>/dev/null;
+cd public_html;
+composer install -n;
+cd ..;
 sudo docker build -t yamba:${version} .;
 sudo docker run -d --name yamba -p 80:80 yamba:${version};
