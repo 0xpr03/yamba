175b17b507c914d6dc504b34f2c6a7cfc37f0c17
Aron Heinecke <aron.heinecke@t-online.de>
2018-10-23 00:31:54 +0200

    fix defaults for plugin
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- backend/ts3plugin/src/lib.rs
+++ backend/ts3plugin/src/lib.rs
@@ -7,9 +7,11 @@ use ts3plugin::*;
 
 struct MyTsPlugin;
 
+const PLUGIN_NAME_I: &'static str = env!("CARGO_PKG_NAME");
+
 impl Plugin for MyTsPlugin {
     fn new(api: &mut TsApi) -> Result<Box<MyTsPlugin>, InitError> {
-        api.log_or_print("Inited", "MyTsPlugin", LogLevel::Info);
+        api.log_or_print("Inited", PLUGIN_NAME_I, LogLevel::Info);
         Ok(Box::new(MyTsPlugin))
         // Or return Err(InitError::Failure) on failure
     }
@@ -17,10 +19,10 @@ impl Plugin for MyTsPlugin {
     // Implement callbacks here
 
     fn shutdown(&mut self, api: &mut TsApi) {
-        api.log_or_print("Shutdown", "MyTsPlugin", LogLevel::Info);
+        api.log_or_print("Shutdown", PLUGIN_NAME_I, LogLevel::Info);
     }
 }
 
 create_plugin!(
-    "My Ts Plugin", "0.1.0", "My name", "A wonderful tiny example plugin",
+    PLUGIN_NAME_I, env!("CARGO_PKG_VERSION"), env!("CARGO_PKG_AUTHORS"), "yamba ts3 controller",
     ConfigureOffer::No, false, MyTsPlugin);
