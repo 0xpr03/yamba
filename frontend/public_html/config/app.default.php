228da643b64af1351bbf57e034b868d1082bfde1
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-24 23:34:40 +0200

    Make db connection persistent
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- frontend/public_html/config/app.default.php
+++ frontend/public_html/config/app.default.php
@@ -247,7 +247,7 @@ return [
         'default' => [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
-            'persistent' => false,
+            'persistent' => true,
             'host' => 'database',
             /*
              * CakePHP will use the default DB port based on the driver selected
