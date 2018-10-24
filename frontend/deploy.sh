08ccdd11d695cf8d77e4cb2ae0868c494a03aebb
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-24 20:26:12 +0200

    Replace build process with docker-compose
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- frontend/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git pull && version=$(git rev-parse HEAD) || version=testing;
-echo "Version=${version}";
-sudo docker rm -f yamba_frontend 2>/dev/null;
-sudo docker build -t yamba_frontend:${version} .;
-sudo docker run -d --link="yamba_mariadb:database" --name yamba_frontend -p 80:80 yamba_frontend:${version};
