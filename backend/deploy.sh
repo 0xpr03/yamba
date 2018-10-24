e83cd2f1f7aff0a07a69e1ef8c93bcf76b0b1232
Aron Heinecke <aron.heinecke@t-online.de>
2018-10-24 16:32:06 +0200

    Fix deamon / daemon typos
    Signed-off-by: Oliver Habersetzer <oliver.habersetzer@gmail.com>
    
    (cherry picked from commit 1ef6579c6f8c68710535042acccf278fdb3ad26d)
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -2,7 +2,7 @@
 git pull && version=$(git rev-parse HEAD) || version=testing;
 echo "Version=${version}";
 echo "Building executable... ";
-cd ./deamon/
+cd ./daemon//
 RUST_BACKTRACE=1 cargo build --release
 cd ..
 cd ./ts3plugin//
@@ -12,6 +12,6 @@ if [ ! -d "ts3client" ]; then
     chmod +x ./ts3-dl.sh
     ./ts3-dl.sh
 fi
-sudo docker rm -f yamba 2>/dev/null;
+sudo docker rm -f yamba;
 sudo docker build -t yamba:${version} .;
 sudo docker run -v $(realpath ./ts3client):/opt/ts3 --net="host" --name yamba -p 80:80 yamba:${version};
