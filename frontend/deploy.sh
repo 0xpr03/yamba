a4791d90537bacf2c87cbb0e600c65a873581e0a
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-24 15:47:11 +0200

    Move composer install into Dockerfile build
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,8 +2,5 @@
 git pull && version=$(git rev-parse HEAD) || version=testing;
 echo "Version=${version}";
 sudo docker rm -f yamba_frontend 2>/dev/null;
-cd public_html;
-composer install -n;
-cd ..;
 sudo docker build -t yamba_frontend:${version} .;
-sudo docker run -d --net="host" --name yamba_frontend -p 80:80 yamba_frontend:${version};
+sudo docker run -d --link="yamba_mariadb:database" --name yamba_frontend -p 80:80 yamba_frontend:${version};
