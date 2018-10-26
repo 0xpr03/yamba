1ec1f1b2b3df523567bf701a892073d2981217c4
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-26 11:01:29 +0200

    Surround fpm env vars with single quotes
    This is necessary because fpm wont start with KEY=VAL where VAL is
    false
    
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- frontend/public_html/copyEnvVars.sh
+++ frontend/public_html/copyEnvVars.sh
@@ -2,6 +2,6 @@
 echo "clear_env = no" >> /etc/php/7.0/fpm/pool.d/www.conf;
 while read p;
 do
-    [[ $p == "YAMBA"* ]] || [[ $p == "MYSQL"* ]] && p=${p:6} && echo env[${p/=/] = } >> /etc/php/7.0/fpm/pool.d/www.conf;
+    [[ $p == "YAMBA"* ]] || [[ $p == "MYSQL"* ]] && p=${p:6} && echo env[${p/=/] = \'}\' >> /etc/php/7.0/fpm/pool.d/www.conf;
 done < <(printenv);
 exit 0;
