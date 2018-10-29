36d08774c6b14e60e3deac0acb63a490b1d93b8d
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-10-29 23:46:47 +0100

    Streamline all ids to char(36)
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- dbInit/schema/backend.sql
+++ dbInit/schema/backend.sql
@@ -1,14 +1,14 @@
 -- This file is part of yamba which is released under <GPL3>. See file LICENSE or go to https://www.gnu.org/licenses/gpl.html for full license details.
 
 CREATE TABLE `streams` (
-  `id` char(32) NOT NULL,
+  `id` char(36) NOT NULL,
   `name` varchar(50) NOT NULL,
   `url` varchar(150) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `songs` (
-  `id` char(32) NOT NULL,
+  `id` char(36) NOT NULL,
   `name` varchar(150) NOT NULL,
   `source` varchar(150) NOT NULL,
   `artist` varchar(50) NOT NULL,
@@ -20,14 +20,14 @@ CREATE TABLE `songs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `playlists` (
-  `id` char(32) NOT NULL,
+  `id` char(36) NOT NULL,
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `songs_to_playlists` (
-  `song_id` char(32) NOT NULL,
-  `playlist_id` char(32) NOT NULL,
+  `song_id` char(36) NOT NULL,
+  `playlist_id` char(36) NOT NULL,
   PRIMARY KEY (`song_id`, `playlist_id`),
   FOREIGN KEY (`song_id`) REFERENCES `songs`(`id`),
   FOREIGN KEY (`playlist_id`) REFERENCES `playlists`(`id`)
