fb144868b1c4d8ca77c321020b78c5a43cf28a31
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-11-02 10:37:37 +0100

    Fix queuesadilla database configuration
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- dbInit/dbInit.sh
+++ dbInit/dbInit.sh
@@ -2,11 +2,11 @@
 # This file is part of yamba which is released under <GPL3>. See file LICENSE or go to https://www.gnu.org/licenses/gpl.html for full license details.
 
 # Create queuesadilla database
-mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $YAMBA_QUEUESADILLA_DATABASE";
+mysql --user=root --password=$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE queuesadilla";
 
 for file in /docker-entrypoint-initdb.d/schema/*.sql; do
     echo $file;
-    mysql --user=root --password=$MYSQL_ROOT_PASSWORD < $file $([[ $(echo $file | grep -v "_vendor.sql$") ]] && echo $MYSQL_DATABASE || echo $YAMBA_QUEUESADILLA_DATABASE);
+    mysql --user=root --password=$MYSQL_ROOT_PASSWORD < $file $([[ $(echo $file | grep -v "_vendor.sql$") ]] && echo $MYSQL_DATABASE || echo queuesadilla);
 done
 
 if [[ $YAMBA_DEBUG == true ]]; then
