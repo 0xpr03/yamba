bae2b792ef48d892079879d7277b35c60b4cfc22
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-11-09 18:35:34 +0100

    Adjust database structure and caps types
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- dbInit/schema/backend.sql
+++ dbInit/schema/backend.sql
@@ -1,28 +1,28 @@
 -- This file is part of yamba which is released under <GPL3>. See file LICENSE or go to https://www.gnu.org/licenses/gpl.html for full license details.
 
 CREATE TABLE `titles` (
-  `id` char(32) NOT NULL,
-  `name` varchar(150) NOT NULL,
-  `source` varchar(150) NOT NULL,
+  `id` CHAR(32) NOT NULL,
+  `name` VARCHAR(250) NOT NULL,
+  `source` VARCHAR(255) NOT NULL,
   `last_used` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  `artist` varchar(50),
+  `downloaded` BIT NOT NULL,
+  `artist` VARCHAR(50),
   `length` INT,
-  `downloaded` bit,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `playlists` (
-  `id` char(36) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `keep` bit DEFAULT 0 NOT NULL,
+  `id` CHAR(36) NOT NULL,
+  `name` VARCHAR(50) NOT NULL,
+  `keep` BIT DEFAULT 0 NOT NULL,
   `created` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   `modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `titles_to_playlists` (
-  `title_id` char(32) NOT NULL,
-  `playlist_id` char(36) NOT NULL,
+  `title_id` CHAR(32) NOT NULL,
+  `playlist_id` CHAR(36) NOT NULL,
   PRIMARY KEY (`title_id`, `playlist_id`),
   FOREIGN KEY (`title_id`) REFERENCES `titles`(`id`),
   FOREIGN KEY (`playlist_id`) REFERENCES `playlists`(`id`)
@@ -30,20 +30,22 @@ CREATE TABLE `titles_to_playlists` (
 
 CREATE TABLE `instances` (
   `id` INT AUTO_INCREMENT NOT NULL,
-  `host` varchar(255) NOT NULL,
+  `host` VARCHAR(255) NOT NULL,
   `port` INT(16) UNSIGNED,
-  `identity` varchar(255),
+  `identity` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `autostart` BIT NOT NULL,
   `cid` INT,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255),
-  `autostart` bit NOT NULL,
+  `name` VARCHAR (255) NOT NULL,
+  `password` VARCHAR (255),
+  `autostart` BIT NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `queues` (
   `index` INT AUTO_INCREMENT NOT NULL,
   `instance_id` INT NOT NULL,
-  `title_id` char(32) NOT NULL,
+  `title_id` CHAR(32) NOT NULL,
   PRIMARY KEY (`index`, `instance_id`),
   FOREIGN KEY (`instance_id`) REFERENCES `instances`(`id`),
   FOREIGN KEY (`title_id`) REFERENCES `titles`(`id`)
--- dbInit/schema/frontend.sql
+++ dbInit/schema/frontend.sql
@@ -1,25 +1,25 @@
 -- This file is part of yamba which is released under <GPL3>. See file LICENSE or go to https://www.gnu.org/licenses/gpl.html for full license details.
 
 CREATE TABLE `users` (
-  `id` char(36) NOT NULL,
-  `email` varchar(150) UNIQUE NOT NULL,
-  `password` varchar(150) NOT NULL,
+  `id` CHAR(36) NOT NULL,
+  `email` VARCHAR(255) UNIQUE NOT NULL,
+  `password` CHAR(60) NOT NULL,
   `created` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
   `modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `users_not_confirmed` (
-  `confirmationToken` char(40) NOT NULL,
-  `user_id` char(36) NOT NULL,
+  `confirmationToken` CHAR(40) NOT NULL,
+  `user_id` CHAR(36) NOT NULL,
   PRIMARY KEY (`confirmationToken`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `add_songs_jobs` (
   `backend_token` INT UNSIGNED NOT NULL,
-  `playlist_id` char(36) NOT NULL,
-  `user_id` char(36) NOT NULL,
+  `playlist_id` CHAR(36) NOT NULL,
+  `user_id` CHAR(36) NOT NULL,
   PRIMARY KEY (`backend_token`),
   FOREIGN KEY (`playlist_id`) REFERENCES `playlists`(`id`),
   FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
