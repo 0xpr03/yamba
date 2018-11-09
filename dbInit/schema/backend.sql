a28b877adae1d3fdfc2abbd9029fad0529f51560
Tristan Schönhals <tristan.schoenhals@gmail.com>
2018-11-09 18:36:51 +0100

    Add cid to instances
    Default channel
    
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- dbInit/schema/backend.sql
+++ dbInit/schema/backend.sql
@@ -34,11 +34,9 @@ CREATE TABLE `instances` (
   `port` INT(16) UNSIGNED,
   `identity` VARCHAR(255) NOT NULL,
   `name` VARCHAR(255) NOT NULL,
-  `autostart` BIT NOT NULL,
-  `cid` INT,
-  `name` VARCHAR (255) NOT NULL,
   `password` VARCHAR (255),
   `autostart` BIT NOT NULL,
+  `cid` INT(32),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
