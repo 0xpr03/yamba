1e1b616d1ae2013f411a44ee36baeb543cbd5224
Oliver Habersetzer <oliver.habersetzer@gmail.com>
2018-11-12 11:13:18 +0100

    Remove deployscript and integrate in startscript

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-./deploy.sh
-sudo docker-compose up
+cd backend
+
+if [ ! -d "ts3client" ]; then
+    echo Download teamspeak client...
+    chmod +x ts3-dl.sh
+    ./ts3-dl.sh
+fi
+
+cd ..
+
+sudo docker-compose up --build
