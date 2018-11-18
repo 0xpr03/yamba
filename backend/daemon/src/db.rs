574c599961afb5f1b006a53cda87eba10ae939d7
Aron Heinecke <aron.heinecke@t-online.de>
2018-11-18 22:23:23 +0100

    Add db init function
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- backend/daemon/src/db.rs
+++ backend/daemon/src/db.rs
@@ -18,11 +18,26 @@
 use failure::Fallible;
 
 use mysql::chrono::prelude::NaiveDateTime;
-use mysql::Pool;
+use mysql::error::Error as MySqlError;
+use mysql::{Opts, OptsBuilder, Pool};
 
 use models;
+use SETTINGS;
 
-pub fn save_playlist(playlist: models::Playlist, pool: Pool) -> Fallible<()> {
+pub fn init_pool() -> Fallible<Pool> {
+    let mut builder = OptsBuilder::new();
+    builder
+        .ip_or_hostname(Some(SETTINGS.db.host.clone()))
+        .db_name(Some(SETTINGS.db.db.clone()))
+        .user(Some(SETTINGS.db.user.clone()))
+        .pass(Some(SETTINGS.db.password.clone()))
+        .tcp_port(SETTINGS.db.port);
+    let opts: Opts = builder.into();
+    Ok(Pool::new(opts)?)
+}
+
+/// Save a set of tracks into the DB and return their IDs
+pub fn insert_tracks(tracks: &[Track], pool: Pool) -> Fallible<()> {
     let stmt = pool.prepare("INSERT INTO `` () VALUES (?,?,?,?)")?;
 
     Ok(())
