a4e3c6c1e0d7ac228bf76487b95b4b547ddf62b9
Aron Heinecke <aron.heinecke@t-online.de>
2018-12-27 12:21:24 +0100

    Fix backend shutdown & cleanup entrypoint
    This allows backend to shutdown gracefully & cleans up entrypoint.sh
    pulseaudio is set to less verbose
    
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+set -e
 
 cd $HOME
 rm -rf .pulse/
 mkdir .pulse
 
-pulseaudio --kill
-pulseaudio --exit-idle-time=-1 -vvvv &
+pulseaudio --kill || true
+pulseaudio --exit-idle-time=-1 &
 
 # report warnings
 export GST_DEBUG=2
-echo "running cmd"
 exec "$@"
