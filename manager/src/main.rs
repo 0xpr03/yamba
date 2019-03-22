5adf4dbd7cca0399367aeefa9712ff52138bfdaa
Aron Heinecke <aron.heinecke@t-online.de>
2019-03-22 17:08:01 +0100

    Cleanup & copyright change
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,19 +1,18 @@
 /*
- *  YAMBA middleware
+ *  YAMBA manager
  *  Copyright (C) 2019 Aron Heinecke
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 #[macro_use]
@@ -155,7 +154,7 @@ fn main() -> Fallible<()> {
     let ctrl_c = tokio_signal::ctrl_c().flatten_stream().into_future();
 
     match runtime.block_on(ctrl_c) {
-        Err(e) => {
+        Err(_e) => {
             // first tuple element conains error, but is neither display nor debug..
             error!("Error in signal handler");
             println!("Shutting down daemon..");
@@ -175,7 +174,7 @@ fn create_instance_cmd(
     rt: &mut Runtime,
 ) -> Fallible<()> {
     if let Some(addr) = args.value_of("ts") {
-        let pw = args.value_of("pw");
+        let _pw = args.value_of("pw");
         let cid: Option<i32> = match args
             .value_of("channel")
             .map(|v| v.parse::<i32>().map_err(|e| ParamErr::InvalidTSAddress(e)))
