eb9076015c90b3c98843df9f0a655633c8d112c1
Tristan Schönhals <tristan.schoenhals@gmail.com>
2019-03-26 00:04:37 +0100

    Add usermanagement/auth table structure
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

---
+++ management/src/main/resources/db/migration/V0.0.0__add_authentication.sql
@@ -0,0 +1,50 @@
+CREATE TABLE users
+(
+  id       SMALLSERIAL                         NOT NULL,
+  username VARCHAR(255) UNIQUE                 NOT NULL,
+  enabled  BOOLEAN   DEFAULT TRUE              NOT NULL,
+  password CHAR(60)                            NOT NULL,
+  created  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE groups
+(
+  id   SMALLSERIAL NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE group_members
+(
+  user_id  SMALLINT NOT NULL,
+  group_id SMALLINT NOT NULL,
+  PRIMARY KEY (user_id, group_id),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE
+);
+
+CREATE TABLE authorities
+(
+  id        SMALLSERIAL NOT NULL,
+  authority VARCHAR(63) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE user_authorities
+(
+  user_id      SMALLINT NOT NULL,
+  authority_id SMALLINT NOT NULL,
+  PRIMARY KEY (user_id, authority_id),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (authority_id) REFERENCES authorities (id) ON DELETE CASCADE
+);
+
+CREATE TABLE group_authorities
+(
+  group_id     SMALLINT NOT NULL,
+  authority_id SMALLINT NOT NULL,
+  PRIMARY KEY (group_id, authority_id),
+  FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE,
+  FOREIGN KEY (authority_id) REFERENCES authorities (id) ON DELETE CASCADE
+);
