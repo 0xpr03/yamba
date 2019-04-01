80d96bcbf780bc9f7ef00cee70fb73b0a8ddf97f
Aron Heinecke <aron.heinecke@t-online.de>
2019-04-02 00:12:47 +0200

    Add backtrace to startscript of manager
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- manager/start.sh
+++ manager/start.sh
@@ -4,4 +4,4 @@ if [ -z "$1" ]
     echo "Missing daemon IP"
 fi
 cargo build
-RUST_LOG=info,cargo=warn,manager=trace,jsonrpc_core=trace,jsonrpc=trace cargo run -- -b 127.0.0.1:9000 -c 0.0.0.0:1336 -d $1:1338 -j 0.0.0.0:1337
+RUST_BACKTRACE=1 RUST_LOG=info,cargo=warn,manager=trace,jsonrpc_core=trace,jsonrpc=trace cargo run -- -b 127.0.0.1:9000 -c 0.0.0.0:1336 -d $1:1338 -j 0.0.0.0:1337
