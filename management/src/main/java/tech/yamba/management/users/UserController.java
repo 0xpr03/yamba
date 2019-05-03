0b77d7a8b864ff7d09f4478dbcba0f2c887063e3
Tristan Schönhals <tristan.schoenhals@gmail.com>
2019-05-03 02:14:40 +0200

    Use subscription model in UserController
    Signed-off-by: Tristan Schönhals <tristan.schoenhals@gmail.com>

--- management/src/main/java/tech/yamba/management/users/UserController.java
+++ management/src/main/java/tech/yamba/management/users/UserController.java
@@ -9,47 +9,75 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
 import tech.yamba.db.jooq.tables.pojos.User;
+import tech.yamba.management.websocket.WebSocketService;
 
 
 @Controller
 @RequestMapping(value = "/api/user", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
 public class UserController {
 
 	private final UserService userService;
+	private final WebSocketService webSocketService;
 
 
 	@Autowired
-	public UserController(UserService userService) {
+	public UserController(UserService userService, WebSocketService webSocketService) {
 		this.userService = userService;
+		this.webSocketService = webSocketService;
 	}
 
 
-	@PostMapping("/add")
-	public ResponseEntity<User> addUser(User user) {
-		return new ResponseEntity<>(userService.addUser(user), HttpStatus.CREATED);
+	@PostMapping()
+	public ResponseEntity<User> addUser(@RequestBody User user) {
+		User result = userService.addUser(user);
+		notifyGetUser();
+		return new ResponseEntity<>(result, HttpStatus.CREATED);
 	}
 
 
-	@GetMapping("/get")
-	public ResponseEntity<List<User>> getUser() {
+	@GetMapping()
+	public ResponseEntity<List<User>> getUsers() {
 		return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
 	}
 
 
-	@PutMapping("/update")
-	public ResponseEntity<User> updateUser(User user) {
-		return new ResponseEntity<>(userService.updateUser(user), HttpStatus.OK);
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable short id) {
+		return userService.getUsers()
+				.stream()
+				.filter(user -> user.getId().equals(id))
+				.findAny()
+				.map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 
 
-	@DeleteMapping("/delete")
-	public ResponseEntity deleteUser(User user) {
-		userService.deleteUser(user.getId());
+	@PutMapping("/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable short id, @RequestBody User user) {
+		User result = userService.updateUser(user.setId(id));
+		notifyGetUser();
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity deleteUser(@PathVariable short id) {
+		userService.deleteUser(id);
+		notifyGetUser();
 		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
+
+
+	private void notifyGetUser() {
+		webSocketService.notifySubscribers(UserController.class, methodName -> methodName.startsWith("getUsers"));
 	}
 }
