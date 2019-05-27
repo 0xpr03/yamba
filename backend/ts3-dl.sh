f0efbfbf1d558c851b4a6cb6dedfd4e231d230ae
Aron Heinecke <aron.heinecke@t-online.de>
2019-05-28 00:16:08 +0200

    Upgrade ts3 version
    Signed-off-by: Aron Heinecke <aron.heinecke@t-online.de>

--- backend/ts3-dl.sh
+++ backend/ts3-dl.sh
@@ -3,8 +3,8 @@ if [ -d "ts3client" ]; then
     rm -Rf ts3client
 fi
 rm TeamSpeak3*;
-wget http://dl.4players.de/ts/releases/3.2.3/TeamSpeak3-Client-linux_amd64-3.2.3.run;
-chmod +x TeamSpeak3-Client-linux_amd64-3.2.3.run;
-./TeamSpeak3-Client-linux_amd64-3.2.3.run;
-rm TeamSpeak3-Client-linux_amd64-3.2.3.run;
+wget http://dl.4players.de/ts/releases/3.2.5/TeamSpeak3-Client-linux_amd64-3.2.5.run;
+chmod +x TeamSpeak3-Client-linux_amd64-3.2.5.run;
+./TeamSpeak3-Client-linux_amd64-3.2.5.run;
+rm TeamSpeak3-Client-linux_amd64-3.2.5.run;
 mv TeamSpeak3-Client-linux_amd64 ts3client;
