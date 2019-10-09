605be5ac498ae636f1142c1af5ba3fc8c8faf73a
TurtleTony <tristan.schoenhals@gmail.com>
2019-10-09 19:39:11 +0200

    Add rails management skeleton with devise setup
    Signed-off-by: TurtleTony <tristan.schoenhals@gmail.com>

---
+++ management/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :lockable, :timeoutable, :trackable,
+         # Settings
+         timeout_in: 2.hours
+end
