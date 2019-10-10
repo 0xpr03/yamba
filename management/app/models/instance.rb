0fd8aa19d8f6296e680490a6e442c5842b5c4d4c
TurtleTony <tristan.schoenhals@gmail.com>
2019-10-10 02:25:00 +0200

    Add daemon instance start/stop
    Signed-off-by: TurtleTony <tristan.schoenhals@gmail.com>

--- management/app/models/instance.rb
+++ management/app/models/instance.rb
@@ -1,3 +1,32 @@
 class Instance < ApplicationRecord
   has_secure_token :api_token
+
+  validates :host, presence: true
+  validates :name, presence: true
+  after_create :start_instance
+  after_update :update_instance
+  after_destroy :stop_instance
+
+  def start_instance
+    HTTP.headers(:content_type => "application/json")
+             .post("http://172.18.0.3:1338/instance/start", :json => {
+                 :id => self.id,
+                 :data => {
+                     :TS => self
+                 },
+                 :volume => 0.05
+             })
+  end
+
+  def update_instance
+    stop_instance
+    start_instance
+  end
+
+  def stop_instance
+    HTTP.headers(:content_type => "application/json")
+        .post("http://172.18.0.3:1338/instance/stop", :json => {
+            :id => self.id
+        })
+  end
 end
