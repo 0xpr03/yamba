00c19f90b7b4ad8fa3c8a68280b5d1a9b95ba464
TurtleTony <tristan.schoenhals@gmail.com>
2019-11-02 18:05:16 +0100

    Implement instance status callback
    Signed-off-by: TurtleTony <tristan.schoenhals@gmail.com>

--- management/app/controllers/callback_controller.rb
+++ management/app/controllers/callback_controller.rb
@@ -1,5 +1,5 @@
 class CallbackController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:resolve]
+  skip_before_action :verify_authenticity_token, only: [:resolve, :instance]
 
   def resolve
     data = resolve_params[:data]
@@ -10,9 +10,28 @@ class CallbackController < ApplicationController
     Song.create(data[:data][:Song])
   end
 
+  def instance
+    instance = Instance.find(instance_params[:id])
+    case instance_params[:state]
+    when "Started"
+      instance.running=true
+    when "Running"
+      instance.running=true
+    when "Stopped"
+      instance.running=false
+    else
+      instance.running=false
+    end
+    instance.save
+  end
+
   private
 
     def resolve_params
       params.require(:callback).permit(:ticket, data: [ :state, data: [ Song: [ :name, :artist, :source, :length]]])
+    end
+
+    def instance_params
+      params.require(:callback).permit(:state, :id)
     end
 end
